ad61a916609ba4feda9d0785381b0d1ef6e9691d
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-13 01:40:57 +0530

    feat(auth-routes): Implemented Routes for Authentication System.

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser, loginUser } = require("../controllers/authController");
+const { signup, login, logout } = require("../controllers/authController");
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 module.exports = router;
