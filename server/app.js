2344c170cfadac859aa341ed4cdd2122a875cabd
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-12 21:47:24 +0530

    chore: Imported startupRoutes to app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./routes/authRoutes");
+const startupRoutes = require("./routes/startupRoutes");
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/startups", startupRoutes);
 
 app.get("/", (req, res) => {
     res.send("Hello Founders!!..");
