d579d9bccad3dbcf3ce67451ab58ecdc995425cc
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-13 01:39:54 +0530

    feat(auth-service): Implemented Logic for Signup, Login of User

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,16 +1,57 @@
-const register = async (data) => {
-  // User creation in DB
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const registerUser = async (data) => {
+  const { name, email, password } = data;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new Error("User aldready exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword
+  });
 
   return {
     message: "User Registered.",
-    data,
+    user,
   };
 };
 
-const login = async (data) => {
-  // Verify User + Generate JWT
+const loginUser = async (data) => {
+  const { email, password } = data;
 
-  return "fake-jwt-token";
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error("User not found.");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    throw new Error("Invalid Credentials.");
+  }
+
+  const token = jwt.sign(
+    { id: user._id }, 
+    JWT_SECRET, 
+    { expiresIn: "7d" }
+  );
+
+  return {
+    message: "User Logged In.",
+    token,
+  };
 };
 
-module.exports = { register, login };
+module.exports = { registerUser, loginUser };
