e3218732d67720cea2cafcd725041bff58cffe6f
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-13 09:14:56 +0530

    feat(dashboard-page): Added Page for Startup Dashboard.

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-heading mb-6">Startup Overview</h1>
+
+      <div className="bg-card p-6 rounded-xl shadow">
+        <p className="font-body">
+          Welcome to your startup analytics dashboard.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
