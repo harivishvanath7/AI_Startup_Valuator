91db70ac23c9cc105b0db2991d32743837b8fd30
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-15 12:31:57 +0530

    chore: Added Protected route in dashboard flow of router.

--- client/src/router/AppRouter.jsx
+++ client/src/router/AppRouter.jsx
@@ -3,11 +3,15 @@ import { useParams } from "react-router-dom";
 
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+
 import DashboardLayout from "../layout/DashboardLayout";
 import Dashboard from "../pages/Dashboard";
+
 import StartupCreation from "../pages/StartupCreation";
 import StartupMetricsForm from "../pages/StartupMetricsForm";
 import StartupAnalysis from "../pages/StartupAnalysis";
+
+import ProtectedRoute from "../components/ProtectedRoute";
 
 const AppRouter = () => {
   return (
@@ -15,13 +19,27 @@ const AppRouter = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route element={<DashboardLayout />}>
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Route>
-        <Route path="/startups/create" element={<StartupCreation />} />
-        <Route path="/startups/:startupId/metrics" element={<StartupMetricsFormWrapper />} />
-        <Route path="/startups/:startupId/analysis" element={<StartupAnalysis />}/>
 
+        {/* Protected routes */}
+
+        <Route
+          element={
+            <ProtectedRoute>
+              <DashboardLayout />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/startups/create" element={<StartupCreation />} />
+          <Route
+            path="/startups/:startupId/metrics"
+            element={<StartupMetricsFormWrapper />}
+          />
+          <Route
+            path="/startups/:startupId/analysis"
+            element={<StartupAnalysis />}
+          />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
@@ -33,6 +51,5 @@ const StartupMetricsFormWrapper = () => {
   const { startupId } = useParams();
   return <StartupMetricsForm startupId={startupId} />;
 };
-
 
 export default AppRouter;
