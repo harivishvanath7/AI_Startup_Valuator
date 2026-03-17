7553351021a1baff3d85c377f0036f26d625c1a4
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-03-17 22:08:04 +0530

    update(home-page): Updated tailwind classes in styling Home page/

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,17 +2,17 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-[var(--color-background)] flex flex-col">
+    <div className="min-h-screen bg-background flex flex-col">
 
       {/* Navbar */}
       <nav className="flex justify-between items-center px-10 py-6">
-        <h1 className="text-2xl font-bold font-[var(--font-heading)]">
+        <h1 className="text-2xl font-bold">
           AI Valuator
         </h1>
 
         <div className="flex gap-4">
           <Link to="/login">
-            <button className="px-4 py-2 text-sm border border-[var(--color-dark)] rounded-lg hover:bg-[var(--color-dark)] hover:text-white transition">
+            <button className="px-4 py-2 text-sm border border-dark rounded-lg hover:bg-dark hover:text-white transition">
               Login
             </button>
           </Link>
@@ -22,9 +22,9 @@ const Home = () => {
       {/* Hero Section */}
       <div className="flex flex-1 flex-col items-center justify-center text-center px-6">
 
-        <h1 className="text-5xl md:text-6xl font-[var(--font-heading)] leading-tight">
+        <h1 className="text-5xl md:text-6xl font-(--font-heading) leading-tight">
           Evaluate Your Startup <br />
-          <span className="text-[var(--color-primary)]">
+          <span className="text-primary">
             Like a VC
           </span>
         </h1>
@@ -37,13 +37,13 @@ const Home = () => {
         {/* Buttons */}
         <div className="mt-8 flex gap-4 flex-wrap justify-center">
           <Link to="/signup">
-            <button className="px-8 py-3 bg-[var(--color-primary)] text-black font-semibold rounded-xl shadow-md hover:scale-105 transition">
+            <button className="px-8 py-3 bg-primary text-black font-semibold rounded-xl shadow-md hover:scale-105 transition">
               Start Analysis 🚀
             </button>
           </Link>
 
           <Link to="/login">
-            <button className="px-8 py-3 border border-[var(--color-dark)] rounded-xl hover:bg-[var(--color-dark)] hover:text-white transition">
+            <button className="px-8 py-3 border border-dark rounded-xl hover:bg-dark hover:text-white transition">
               Login
             </button>
           </Link>
