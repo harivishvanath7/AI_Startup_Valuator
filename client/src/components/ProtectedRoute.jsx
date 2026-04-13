e7faa8de0dd637868ee93798057861d7f3bba1c4
HARI VISHVANATH S <harivishvanath7@gmail.com>
2026-04-13 14:36:40 +0530

    feat: added return statement.

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,14 +1,13 @@
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoute = () => {
-
+const ProtectedRoute = ({ children }) => {
     const token = localStorage.getItem("token");
 
     if (!token) {
         return <Navigate to="/login" />;
     }
 
-    return <Outlet />;
+    return children ? children : <Outlet />;
 }
 
 export default ProtectedRoute;
